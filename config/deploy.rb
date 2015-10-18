4d94b3fe8e0a0333923d8715d012f719b976c766
Zeeshan Mahmood <zeeshan.mahmood@7vals.com>
2015-10-18 14:07:10 +0500

    deploymnt

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,8 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+
+SSHKit.config.command_map[:rake]  = "bundle exec rake"
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
@@ -75,7 +77,6 @@ namespace :deploy do
   end
 
   before :starting,     :check_revision
-  after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
